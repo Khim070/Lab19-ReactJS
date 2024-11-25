b9fc38f15e7b4f363781c43581bb7d3f0d871584
Khim070 <Vakhiml60@gmail.com>
2024-11-25 12:37:59 +0700

    update
    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import $ from 'jquery';
+window.$ = $;
 
+// ReactDOM.render(<App />, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
